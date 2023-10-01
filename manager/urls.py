7e99f8907005690cba6e8e9b9ccc6789ae0cfcbf
anna-lybid <anna.lybid@gmail.com>
2023-10-01 16:27:37 +0300

    Add List and Detail Views for every model, create basic page template for them

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from manager.views import index
+from manager.views import (
+    index,
+    TaskListView,
+    TaskDetailView,
+    WorkerListView,
+    WorkerDetailView,
+    PositionListView,
+    PositionDetailView,
+    TaskTypeListView,
+    TaskTypeDetailView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/<int:pk>/", PositionDetailView.as_view(), name="position-detail"),
+    path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task-types/<int:pk>/", TaskTypeDetailView.as_view(), name="task-type-detail"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,54 @@
 from django.shortcuts import render
+from django.views import generic
+
+from manager.models import Task, Worker, Position, TaskType
 
 
 def index(request):
     """View function for the home page of the site."""
     return render(request, "manager/index.html")
 
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "manager/task_list.html"
+    paginate_by = 10
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    template_name = "manager/task_detail.html"
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    context_object_name = "worker_list"
+    template_name = "manager/worker_list.html"
+    paginate_by = 10
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+    template_name = "manager/worker_detail.html"
+
+
+class PositionListView(generic.ListView):
+    model = Position
+    template_name = "manager/position_list.html"
+    paginate_by = 10
+
+
+class PositionDetailView(generic.DetailView):
+    model = Position
+    template_name = "manager/position_detail.html"
+
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    template_name = "manager/task_type_list.html"
+    paginate_by = 10
+
+
+class TaskTypeDetailView(generic.DetailView):
+    model = TaskType
+    template_name = "manager/task_type_detail.html"
