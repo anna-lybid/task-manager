c2ba06db8f6265829bd84bbe753afad57735344f
anna-lybid <anna.lybid@gmail.com>
2023-10-04 16:20:12 +0300

    Update settings & requirements.txt

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dxm-4_r$!&1&ye6-pexvecv26npyl4-8oj$c*3x*kpkkuue16^"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-dxm-4_r$!&1&ye6-pexvecv26npyl4-8oj$c*3x*kpkkuue16^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") == "False"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
