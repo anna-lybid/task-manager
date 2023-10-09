ff1eac947ced5b8f642d88fd48a2edabf27ca199
anna-lybid <anna.lybid@gmail.com>
2023-10-09 22:45:40 +0300

    Add onrender host to allowed_hosts in settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -17,8 +17,10 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "task-manager-for-an-it-company.onrender.com"
+]
 
 # Application definition
 
